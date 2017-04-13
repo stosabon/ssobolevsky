31328074a1543142509b108d8200ec9b09a770a7
stosabon <stosabon@mail.ru>
2017-04-13 17:11:36 +0300

    Add Java Doc

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j;
-
+/**
+* Class Класс для вывода надписи на консоль
+* @author ssobolevsky
+* @since 11.04.2017
+* @version 1
+*/
 public class Calculate{
+	/**
+	* Конструктор, вывод строки в консоль
+	* @param arg - arg
+	*/
     public static void main(String[] args){
 	System.out.println("Hello World");
     }
