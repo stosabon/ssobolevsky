f17d99abd1a3dc833a6017b6e2fb64c5cf7b24ca
stosabon <stosabon@mail.ru>
2017-05-12 12:04:46 +0300

    exceptions

--- chapter_002/src/main/java/ru/job4j/start/StubInput.java
+++ chapter_002/src/main/java/ru/job4j/start/StubInput.java
@@ -34,6 +34,6 @@ public class StubInput implements Input {
 
     @Override
     public int ask(String question, int[] range) {
-        return 0;
+        return Integer.valueOf(this.answers[position++]);
     }
 }
