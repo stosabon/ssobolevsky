c82baddd661216b1e53777acab82731096ab9855
stosabon <stosabon@mail.ru>
2017-06-17 17:55:14 +0300

    store

--- chapter_004/src/main/java/ru/job4j/generics/RoleStore.java
+++ chapter_004/src/main/java/ru/job4j/generics/RoleStore.java
@@ -3,37 +3,6 @@ package ru.job4j.generics;
 /**
  * Created by pro on 15.06.2017.
  */
-public class RoleStore<T extends Base> implements Store<T> {
-    /**
-     * Container.
-     */
-    private SimpleArray<T> objects;
+public class RoleStore extends AbstractStore<Role> {
 
-    /**
-     * Method to add to container.
-     * @param value - value
-     */
-    @Override
-    public void add(T value) {
-        this.objects.add(value);
-    }
-
-    /**
-     * Method to update object.
-     * @param index - index
-     * @param value - value
-     */
-    @Override
-    public void update(int index, T value) {
-        this.objects.update(index,value);
-    }
-
-    /**
-     * Method to remove object.
-     * @param index - index
-     */
-    @Override
-    public void remove(int index) {
-        this.objects.remove(index);
-    }
 }
