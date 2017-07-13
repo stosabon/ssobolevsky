904e20bfbf7a4862a0ceed75fe9140dc186b43a2
stosabon <stosabon@mail.ru>
2017-07-13 14:49:34 +0300

    1. Создать модель User [#999]

---
+++ chapter_004/src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,21 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+/**
+ * Created by pro on 13.07.2017.
+ */
+public class User {
+    /**
+     * name of user.
+     */
+    private String name;
+    /**
+     * Age of user.
+     */
+    private int children;
+    /**
+     * Birth date of user.
+     */
+    private Calendar birthday;
+}
---
+++ chapter_004/src/main/java/ru/job4j/map/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Map training.
+ * Created by pro on 13.07.2017.
+ */
+package ru.job4j.map;
